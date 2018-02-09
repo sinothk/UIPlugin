8bdec422a84444a513d8a29c63b935e0f5cb7207
SINOTHK\LiangYT <381518188@qq.com>
2018-02-09 09:42:21 +0800

    加入SwitchMultiButton

---
+++ app/src/main/java/com/sinothk/ui/demo/switchMultiButton/SwitchMultiButtonMainActivity.java
@@ -0,0 +1,37 @@
+package com.sinothk.ui.demo.switchMultiButton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.sinothk.ui.demo.R;
+import com.sinothk.ui.switchMultiButton.SwitchMultiButton;
+
+public class SwitchMultiButtonMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //set switch tabs with ***app:switchTabs*** in xml
+        SwitchMultiButton switchmultibutton = (SwitchMultiButton) findViewById(R.id.switchmultibutton);
+        switchmultibutton.setOnSwitchListener(new SwitchMultiButton.OnSwitchListener() {
+            @Override
+            public void onSwitch(int position, String tabText) {
+                Toast.makeText(SwitchMultiButtonMainActivity.this, tabText, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //or set switch tabs in java code
+        SwitchMultiButton mSwitchMultiButton2 = (SwitchMultiButton) findViewById(R.id.switchmultibutton2);
+        mSwitchMultiButton2.setText("点个Star", "狠心拒绝").setOnSwitchListener(new SwitchMultiButton.OnSwitchListener() {
+            @Override
+            public void onSwitch(int position, String tabText) {
+                Toast.makeText(SwitchMultiButtonMainActivity.this, tabText, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        mSwitchMultiButton.setSelectedTab(0);
+    }
+}
